2893246683ce840d21dfb2196d631023d5f035de
jamesasah <asahjames223@gmail>
2025-01-23 15:09:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import FeatureSection from "./components/FeatureSection"
+import Footer from "./components/Footer"
+import Herosection from "./components/Herosection"
+import Navbar from "./components/navbar"
+import Pricing from "./components/Pricing"
+import TestimonialSection from "./components/TestimonialSection"
+import Workflow from "./components/Workflow"
+
+const App = () => {
+  return (
+    <div>
+     <Navbar/>
+     <div className="max-w-7xl mx-auto pt-20 px-6">
+     <Herosection/>
+     <FeatureSection/>
+     <Workflow/>
+     <Pricing/>
+    <TestimonialSection/>
+    <Footer/>
+     </div>
+    </div>
+  )
+}
+
+export default App
